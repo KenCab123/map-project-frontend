c0a08f38b7f4460c14680f5471dc3286bc90a2e8
Timothy Natal <timothynatal@pursuit.org>
2024-05-18 15:19:28 -0400

    Map component and styling added

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,19 @@
-import React, { useEffect, useRef } from "react";
-import L from 'leaflet';
-import geoJSONData from "../assets/geoJSON/boroughs.json";
-import { changeColor } from "./helpers/mapFunctions";
+import React from 'react'
+import Map from "./Components/Map"
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
-  const mapRef = useRef(null);
-  let defaultStyle = null; // Variable to store default style
 
-  useEffect(() => {
-    if (!mapRef.current) {
-      // Create the map
-      const map = L.map('map', {
-        zoomControl: false, // Disable zoom control
-        doubleClickZoom: false, // Disable zoom on double-click
-        scrollWheelZoom: false, // Disable zoom with scroll wheel
-        touchZoom: false, // Disable touch zoom
-        dragging: false // Disable dragging
-      }).setView([40.7128, -74.0060], 10);
-
-      // Add GeoJSON layer for the boroughs with custom styling
-      const geojson = L.geoJSON(geoJSONData, {
-        style:style,
-        onEachFeature: onEachFeature
-      }).addTo(map);
-
-      function style(feature,percentage = 10) {
-
-        return {
-            fillColor: changeColor(percentage),
-            weight: 2,
-            opacity: 1,
-            color: 'white',
-            dashArray: '3',
-            fillOpacity: 0.7
-        };
-    }
-
-    function highlightFeature(e) {
-      const layer = e.target;
-  
-      layer.setStyle({
-          weight: 5,
-          color: '#666',
-          dashArray: '',
-          fillOpacity: 0.7
-      });
-  
-      layer.bringToFront();
-  }
-
-  function resetHighlight(e) {
-    geojson.resetStyle(e.target);
-}
-
-      function onEachFeature(feature, layer) {
-        layer.on({
-            mouseover: highlightFeature,
-            mouseout: resetHighlight,
-        });
-    }
-      // Store the map instance in the ref
-      mapRef.current = map;
-    }
-  }, []); // Empty dependency array ensures this runs only once
 
   return (
-    <div>
-      <div id="map" style={{ width: '100%', height: '500px' }}></div>
-    </div>
-  );
-};
 
-export default App;
+    <div>
+      <Routes>
+        <Route path="/"element={<Map />}>
+        </Route>
+      </Routes>
+      </div>
+  )
+}
+
+export default App
