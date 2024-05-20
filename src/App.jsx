4ea90ca5208ffeae4d4c3c7b94b1083ffbbb2df6
Timothy Natal <timothynatal@pursuit.org>
2024-05-20 11:29:33 -0400

    updated modal styling

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ const App = () => {
   return (
 
     <div className='page-wrap'>
+      <h1 className='app-name'>BulletProof NYC</h1>
       <Routes>
         <Route path="/"element={<Map />}>
         </Route>
